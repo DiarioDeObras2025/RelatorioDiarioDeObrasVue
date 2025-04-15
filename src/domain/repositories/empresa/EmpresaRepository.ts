5ed189389c6180f486d5749888bcaeb35ffb5656
leonardo <leo.rodrigues.profissional@gmail.com>
2025-04-15 12:44:46 -0300

    ajsut

--- src/domain/repositories/empresa/EmpresaRepository.ts
+++ src/domain/repositories/empresa/EmpresaRepository.ts
@@ -2,7 +2,7 @@ import api from "@/config/axios";
 import type { Empresa } from "@/domain/entities/empresa/Empresa";
 import type { IEmpresaRepository } from "@/domain/interface/empresa/IEmpresaRepository";
 
-const apiUrl = "https://localhost:7159/empresa/create-empresa";
+const apiUrl = `${import.meta.env.VITE_API_URL}/empresa/create-empresa`;
 
 export class EmpresaRepository implements IEmpresaRepository {
   createEmpresa(empresa: Empresa): Promise<any> {
